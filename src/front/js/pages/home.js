dc75278c4cb1201667955935da2040df6c291287
Sara uri <146222294+Suri1336@users.noreply.github.com>
2024-03-15 15:52:46 +0000

    home page done

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,26 +1,36 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
-import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
 
 export const Home = () => {
-	const { store, actions } = useContext(Context);
+  const { store, actions } = useContext(Context);
 
-	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
-			<div className="alert alert-info">
-				{store.message || "Loading message from the backend (make sure your python backend is running)..."}
-			</div>
-			<p>
-				This boilerplate comes with lots of documentation:{" "}
-				<a href="https://start.4geeksacademy.com/starters/react-flask">
-					Read documentation
-				</a>
-			</p>
-		</div>
-	);
+  return (
+    <div className="container- text-center ">
+       {/* jumbotron for home page start here */}
+      <div class=" p-5  jumbotron gradient text-white rounded">
+        <h1 className="Welcome pt-5"> Welcome To Color Me</h1>
+        <button className="purple btn mt-3 mb-5 text-light">Start Here</button>
+      </div>
+      {/* cards for home page start here */}
+      <div className="row mt-5 d-flex justify-content-evenly">
+        <div class="card pink p-5" style={{ width: "18rem" }} >
+          <div class="card-body p-5">
+            <h2 class="card-title text-light" >123</h2>
+          </div>
+        </div>
+        <div class="card orange p-5" style={{ width: "18rem" }} >
+          <div class="card-body p-5">
+            <h2 class="card-title text-light">123</h2>
+          </div>
+        </div>
+        <div class="card yellow p-5" style={{ width: "18rem" }} >
+          <div class="card-body p-5">
+            <h2 class="card-title text-light">123</h2>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  );
 };
