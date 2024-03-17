8ed517b8db90b8631538bbb5bf22d6b603475f1f
Sara uri <146222294+Suri1336@users.noreply.github.com>
2024-03-17 14:53:35 +0000

    sign up done with no button

--- src/front/js/pages/signup.js
+++ src/front/js/pages/signup.js
@@ -2,15 +2,27 @@ import React, { useContext } from "react";
 import { Context } from "../store/appContext";
 import "../../styles/signup.css";
 
-export const Signup= () => {
+export const Signup = () => {
   const { store, actions } = useContext(Context);
   return (
     <div className="text-white signup">
-        <div   className="text-white rounded">
-          {/* form starts here do css in signup */}
-        <h1 > Welcome To Color Me</h1>
-        <button className="btn  text-light">Sign Up</button>
+      <div className="text-white rounded">
+        {/* form starts here do css in signup */}
+        <form className="Application" method="POST">
+          <div className="inputContainer">
+            <input type="text" name="Username" placeholder="Username" className="input" />
+            <label className="label">Username</label>
+          </div>
+          <div className="inputContainer">
+            <input type="text" name="Password" placeholder="Password" className="input" />
+            <label className="label">Password</label>
+          </div>
+          <input type="submit" name="submit" value="login" className="button btn-light text-dark"/>
+          
+          
+        </form>
+
       </div>
-        <div> this is the signup page </div>
     </div>
-  )}
+  )
+}
