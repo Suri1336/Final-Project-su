95af6591570b9851924a19e11bbcda7984982c43
Sara uri <146222294+Suri1336@users.noreply.github.com>
2024-03-21 05:51:32 +0000

    img size done

--- src/front/js/pages/sheets.js
+++ src/front/js/pages/sheets.js
@@ -26,8 +26,14 @@ import KawaiiUrl from "../../img/kawaii1-img.jpeg"
 export const Sheets = () => {
   const { store, actions } = useContext(Context);
   return (
-    <div className="Sheets">
-      <div className="card cardSheets" style={{ width: "18rem" }}>
+    <div className="Sheets row align-items-start">
+      <div className="card cardSheets col">
+        <img src={KawaiiUrl} className="card-img-top" alt="hi" />
+        <div className="card-body">
+          <p className="card-text">text</p>
+        </div>
+      </div>
+      <div className="card cardSheets col">
         <img src={KawaiiUrl} className="card-img-top" alt="hi" />
         <div className="card-body">
           <p className="card-text">text</p>
