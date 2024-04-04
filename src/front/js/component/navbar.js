04b6f7467efe941430adc688df2879333afe3a27
Sara uri <146222294+Suri1336@users.noreply.github.com>
2024-04-04 22:31:09 +0000

    um its working

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -9,13 +9,10 @@ export const Navbar = () => {
 		<nav className="navbar navbar-expand-lg bg-body-tertiary purple ">
 			<div className="container-fluid">
 				<a className="navbar-brand" href="#">Color Me</a>
-				<button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-					<span className="navbar-toggler-icon"></span>
-				</button>
 				<div className="collapse navbar-collapse" id="navbarSupportedContent">
 					<ul className="navbar-nav me-auto mb-2 mb-lg-0">
 						<li className="nav-item">
-							<Link className="nav-link active" to="/">Home</Link>
+							<Link className="nav-link active text-light" to="/">Home</Link>
 						</li>
 						<li className="nav-item dropdown">
 							<a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
@@ -36,10 +33,6 @@ export const Navbar = () => {
 							}
 						</li>
 					</ul>
-					<form className="d-flex" role="search">
-						<input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-						<button className="btn btn-outline-success" type="submit">Search</button>
-					</form>
 
 				</div>
 			</div>
